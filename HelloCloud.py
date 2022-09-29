d7c3aae4adc1293e3abc590a8a5c0b954bffa4cb
puntita <6406022620088@fitm.kmutnb.ac.th>
2022-09-29 16:14:56 +0700

    up

--- HelloCloud.py
+++ HelloCloud.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello! RUK-COM</h1>'
+    return '<h1>Hello! RUK-COM is Great !</h1>'
 
 
 @app.route('/home', methods=['GET', 'POST'])
