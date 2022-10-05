6c6d0633594162c1710f245e93a543998ab29932
puntita <6406022620088@kmutnb.ac.th>
2022-10-05 22:49:43 +0700

    l

--- L05/Ex1models.py
+++ L05/Ex1models.py
@@ -1,8 +1,8 @@
-from unicodedata import name
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'users'
