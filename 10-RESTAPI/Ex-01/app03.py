d2f554e3deae4a4bb14723efd81141f9cff571d8
puntita <6406022620088@fitm.kmutnb.ac.th>
2022-10-20 15:42:25 +0700

    s

--- 10-RESTAPI/Ex-01/app03.py
+++ 10-RESTAPI/Ex-01/app03.py
@@ -1,24 +1,21 @@
-from unicodedata import name
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from matplotlib.pyplot import cla
-from numpy import product
 from flask_marshmallow import Marshmallow
 import os
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_DATABASE_URI'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 ma = Marshmallow(app)
 
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_ker=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     description = db.Column(db.String(200))
     price = db.Column(db.Float)
